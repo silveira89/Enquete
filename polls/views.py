4ee4e96eb96ebc89e1e4db0c8069223f6e33fc73
Andre Silveira <alasandre@gmail.com>
2023-09-27 22:25:01 -0300

    Alterações nos arquivos settings, admin, models e views.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-
-# Create your views here.
